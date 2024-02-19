b1f8b2a61cf5ccc680de71cf78b4c8891ecbf1c8
Jaykumar Pokar <jaykumar.pokar@coditas.com>
2024-02-19 17:37:08 +0530

    Completed till string

---
+++ js full course part1/1)javascript_basic.js
@@ -0,0 +1,26 @@
+alert('Hi');
+
+alert('Good jobs!');
+
+2 + 2;
+
+10 - 3;
+
+document.body.innerHTML = 'hello';
+
+// Excercise
+
+// 1a)
+alert('Good morning!');
+
+// 1b)
+alert("Jaykumar");
+
+// 1c)
+console.log(10 + 5);
+
+// 1d)
+console.log(20 - 5);
+
+// 1e)
+console.log(2 + 2 - 5);
---
+++ js full course part1/3)strings.js
@@ -0,0 +1,73 @@
+'hi' + 'hello' + 'hey';
+
+typeof 2;
+typeof 'hey';
+
+'hey' + 3;
+
+'$' + 20.95 + 7.99;
+
+'$' + (20.95 + 7.99);
+
+'$' + ((2095 + 799) / 100);
+
+'items (' + (1 + 1) + '): $' + (2095 + 799) / 100
+
+alert('items (' + (1 + 1) + '): $' + (2095 + 799) / 100);
+
+
+"I'm going to play vollyball"
+
+'I\'m going to play vollyball'
+
+'my name is \n Jaykumar'
+
+    `${112 + 32}`
+
+    `Items (${1 + 1}): $${(2095 + 799) / 100}`
+
+
+// Excercise
+
+// 3a)
+'My name is ';
+
+// 3b)
+'Jaykumar';
+
+// 3c)
+'My name is ' + 'Jaykumar';
+
+// 3d)
+'Total cost: $' + (5 + 3);
+
+
+// 3e)
+`Total cost: $ ${5 + 3}`;
+
+// 3f)
+alert(`Total cost: $ ${5 + 3}`);
+
+// 3g)
+`Total cost: $  ${((599 + 295) / 100)}`;
+
+// 3h)
+`Total cost: $  ${((599 + 295) / 100)}`;
+
+// 3i)
+alert(`Total cost: $  ${((599 + 295) / 100)}`);
+
+// 3j)
+alert(`Total cost: $${(599 + 295) / 100} Do visit again...`);
+
+// 3k)
+`Items ${2 + 2}: $ ${((2 * 20.95) + (2 * 7.99))}`
+
+    // 3l)
+    `shipping: $${(4.99 + 4.99)}`
+
+    // 3m)
+    `Total before tax: $${(2 * 20.95 + 2 * 7.99 + 4.99 + 4.99)}`
+
+    // 3n)
+    `Estimated tax (10%) : $${Math.round(((2 * 20.95) + (2 * 7.99) + 4.99 + 4.99) * (10 / 100))}`
